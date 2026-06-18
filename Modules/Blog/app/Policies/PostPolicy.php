cf4b92abdad1a3288dcb4294cf80f3bb3d1823b5
Yuka <128908897+yukazakiri@users.noreply.github.com>
2026-06-18 09:20:39 +0800

    chore: add Herd guidance and mail package

--- Modules/Blog/app/Policies/PostPolicy.php
+++ Modules/Blog/app/Policies/PostPolicy.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace Modules\Blog\Policies;
 
-use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Foundation\Auth\User as AuthUser;
 use Modules\Blog\Models\Post;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
 class PostPolicy
 {
     use HandlesAuthorization;
-
+    
     public function viewAny(AuthUser $authUser): bool
     {
         return $authUser->can('ViewAny:Post');
@@ -66,4 +66,5 @@ class PostPolicy
     {
         return $authUser->can('Reorder:Post');
     }
+
 }
--- app/Policies/CurriculumPolicy.php
+++ app/Policies/CurriculumPolicy.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Policies;
 
+use Illuminate\Foundation\Auth\User as AuthUser;
 use App\Models\Curriculum;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Illuminate\Foundation\Auth\User as AuthUser;
 
 class CurriculumPolicy
 {
     use HandlesAuthorization;
-
+    
     public function viewAny(AuthUser $authUser): bool
     {
         return $authUser->can('ViewAny:Curriculum');
@@ -66,4 +66,5 @@ class CurriculumPolicy
     {
         return $authUser->can('Reorder:Curriculum');
     }
+
 }
