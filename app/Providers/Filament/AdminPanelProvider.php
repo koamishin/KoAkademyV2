606c502e9d7e517508179738b18a4a1741b603c1
SorenOutis <poweredbyrazer022@gmail.com>
2026-06-23 01:38:33 +0800

    style: reorder and add filament admin imports
    clean up import order in the filament admin provider, prepare for using filament's color support, and update submodule reference to reflect uncommitted changes in ui-ux-pro-max-skill

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -20,10 +20,10 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
 use Filament\View\PanelsRenderHook;
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
-use FinityLabs\FinMail\FinMailPlugin;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -32,6 +32,7 @@ use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use RobertBoes\FilamentPasskeys\FilamentPasskeysPlugin;
+use FinityLabs\FinMail\FinMailPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
